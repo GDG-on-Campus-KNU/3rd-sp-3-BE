932eb6d9bfa721a4d0ec7a9b4257287cf9142a57
Gyumin Sim <gyu2781@gmail.com>
2024-04-13 02:16:18 +0900

    #1 feat : service - 채팅 저장, 채팅 로그 가져오기 메소드 작성

---
+++ src/main/java/gdsc/comunity/service/ChatService.java
@@ -0,0 +1,40 @@
+package gdsc.comunity.service;
+
+import gdsc.comunity.controller.chat.PagingChatting;
+import gdsc.comunity.dto.chat.Chatting;
+import gdsc.comunity.entity.chat.ChatLog;
+import gdsc.comunity.repository.channel.ChannelRepository;
+import gdsc.comunity.repository.chat.ChatRepository;
+import gdsc.comunity.repository.user.UserChannelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+    private final ChatRepository chatRepository;
+    private final ChannelRepository channelRepository;
+    private final UserChannelRepository userChannelRepository;
+
+    @Async
+    public void saveChat(Chatting chatting, Long channelId) {
+        var channel = channelRepository.findById(channelId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 채널을 찾을 수 없습니다."));
+
+        Long userId = userChannelRepository.getUserIdByNicknameAndChannel(chatting.senderNickname, channel);
+
+        chatRepository.save(chatting.toEntity(channel, userId));
+    }
+
+    public PagingChatting getChatLog(Long channelId, int page) {
+        Page<ChatLog> chatLogPage = chatRepository.findChatLogByPage(channelId, page);
+
+        Map<Long, String> nicknameMap = userChannelRepository.getNicknameMapByChannelId(channelId);
+
+        return PagingChatting.of(chatLogPage, nicknameMap);
+    }
+}
