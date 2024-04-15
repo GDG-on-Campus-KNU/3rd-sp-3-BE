03b8d97a49ba79f1a09a9b951324e5bad4d2447d
jaehyeon <fanta4715@naver.com>
2024-04-15 19:10:08 +0900

    #5 [Feat] OAuth 로그인 구현

--- src/main/java/gdsc/comunity/repository/user/UserRepository.java
+++ src/main/java/gdsc/comunity/repository/user/UserRepository.java
@@ -1,7 +1,9 @@
 package gdsc.comunity.repository.user;
 
 import gdsc.comunity.entity.user.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/gdsc/comunity/service/OAuthService.java
@@ -0,0 +1,103 @@
+package gdsc.comunity.service;
+
+import gdsc.comunity.dto.JwtTokensDto;
+import gdsc.comunity.entity.user.Provider;
+import gdsc.comunity.entity.user.User;
+import gdsc.comunity.repository.user.UserRepository;
+import gdsc.comunity.security.jwt.JwtProvider;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OAuthService {
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String CLIENT_ID;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String CLIENT_SECRET;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String REDIRECT_URI;
+    private final JwtProvider jwtProvider;
+    private final WebClient webClientForGetAccessToken = WebClient.builder()
+            .baseUrl("https://oauth2.googleapis.com")
+            .build();
+    private final WebClient webClientForGetUserInfo = WebClient.builder()
+            .baseUrl("https://www.googleapis.com/userinfo/v2/me")
+            .build();
+    private final UserRepository userRepository;
+
+    @Transactional
+    public String getAccessToken(String code) {
+        Map<String, String> responseBody;
+        // OAuth 토큰 요청 보내기
+        log.info("getAccessToken method start, code : {}", code);
+
+        responseBody = webClientForGetAccessToken.post()
+                .uri("token")
+                .headers(httpHeaders -> {
+                    httpHeaders.set("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+                })
+                .body(BodyInserters.fromFormData("code", code)
+                        .with("client_id", CLIENT_ID)
+                        .with("client_secret", CLIENT_SECRET)
+                        .with("redirect_uri", REDIRECT_URI)
+                        .with("grant_type", "authorization_code"))
+                .retrieve()
+                .toEntity(Map.class)
+                .block().getBody();
+
+        log.info("getAccessToken method end soon, code : {}", code);
+        return responseBody.get("access_token");
+    }
+
+    public Map<String, String> getUserInfoByAccessToken(String accessTokenByOAuth) {
+        Map<String, String> responseBody;
+        // OAuth 토큰으로 유저 정보 요청 보내기
+        log.info("getUserInfoByAccessToken method start, accessTokenByOAuth : {}", accessTokenByOAuth);
+
+        responseBody = webClientForGetUserInfo.get()
+                .uri("")
+                .headers(httpHeaders -> {
+                    httpHeaders.set("Authorization", "Bearer " + accessTokenByOAuth);
+                })
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .toEntity(Map.class)
+                .block().getBody();
+
+        log.info("getUserInfoByAccessToken method end soon, accessTokenByOAuth : {}", accessTokenByOAuth);
+        return responseBody;
+    }
+
+    @Transactional
+    public JwtTokensDto loginOrRegister(Map<String, String> userInfo) {
+        // 유저가 존재하는지 확인
+        String email = userInfo.get("email");
+        Optional<User> userOP= userRepository.findByEmail(email);
+        if (userOP.isEmpty()) {
+            // 유저가 존재하지 않으면 회원가입
+            User user = User.builder()
+                    .providerId(userInfo.get("id"))
+                    .profileImageUrl(userInfo.get("picture"))
+                    .provider(Provider.GOOGLE)
+                    .email(email)
+                    .build();
+            User savedUser = userRepository.save(user);
+            return jwtProvider.generateTokens(savedUser.getId());
+        }
+        return jwtProvider.generateTokens(userOP.get().getId());
+    }
+}
