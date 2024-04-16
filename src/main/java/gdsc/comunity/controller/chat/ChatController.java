d2b127d241f11f12a18135400a3d0b3e57364daa
Gyumin Sim <gyu2781@gmail.com>
2024-04-17 02:54:17 +0900

    #1 fix : websocket api에서 @PathVariable -> @DestinationVariable로 변경

--- src/main/java/gdsc/comunity/controller/chat/ChatController.java
+++ src/main/java/gdsc/comunity/controller/chat/ChatController.java
@@ -5,6 +5,7 @@ import gdsc.comunity.dto.chat.PagingChatting;
 import gdsc.comunity.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,20 +19,22 @@ public class ChatController {
     private final ChatService chatService;
 
     @MessageMapping("/{channelId}/{userNickname}")
-    public void enter(@PathVariable Long channelId, @PathVariable String userNickname){
+    public void enter(@DestinationVariable Long channelId,
+                      @DestinationVariable String userNickname){
         messagingTemplate.convertAndSend("/api/sub/" + channelId
                 , userNickname);
     }
 
     @MessageMapping("/{channelId}")
-    public void chat(Chatting chatting, @PathVariable Long channelId) {
+    public void chat(Chatting chatting, @DestinationVariable Long channelId) {
         chatService.saveChat(chatting, channelId);
         messagingTemplate.convertAndSend("/api/sub/" + channelId
                 , chatting);
     }
 
     @GetMapping("/api/chatLog/{channelId}/{page}")
-    public ResponseEntity<PagingChatting> getChatLog(@PathVariable Long channelId, @PathVariable int page){
+    public ResponseEntity<PagingChatting> getChatLog(@PathVariable Long channelId,
+                                                     @PathVariable int page){
         return ResponseEntity.ok(chatService.getChatLog(channelId, page));
     }
 
