728bedd404ebf15454a46713bc161608e77f8a93
Gyumin Sim <gyu2781@gmail.com>
2024-04-17 02:59:15 +0900

    #1 fix : stomp 설정 에러 수정

--- src/main/java/gdsc/comunity/configuration/WebSocketConfigurer.java
+++ src/main/java/gdsc/comunity/configuration/WebSocketConfigurer.java
@@ -19,8 +19,7 @@ public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
 }
