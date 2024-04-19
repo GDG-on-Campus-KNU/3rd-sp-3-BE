f207ad2bfbfa2bf068c0e52ecabb3578e94dee55
Gyumin Sim <gyu2781@gmail.com>
2024-04-19 17:19:07 +0900

    #1 style : stomp mapping url /ws 에서 /stomp로 변경

--- src/main/java/gdsc/comunity/configuration/WebSocketConfigurer.java
+++ src/main/java/gdsc/comunity/configuration/WebSocketConfigurer.java
@@ -18,7 +18,7 @@ public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/stomp")
                 .setAllowedOrigins("*");
     }
 
