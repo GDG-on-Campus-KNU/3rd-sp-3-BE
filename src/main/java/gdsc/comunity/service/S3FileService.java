d5911a84e64ffa088a4f06c56065c58b4efbb646
Gyumin Sim <gyu2781@gmail.com>
2024-05-26 00:07:45 +0900

    #9 fix - S3 버킷 액세스 키를 사용해서 접근하도록 변경 - 메타데이터 사용해서 multipart 파일 그대로 업로드하도록 변경

--- src/main/java/gdsc/comunity/service/S3FileService.java
+++ src/main/java/gdsc/comunity/service/S3FileService.java
@@ -1,14 +1,18 @@
 package gdsc.comunity.service;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import gdsc.comunity.exception.CustomException;
 import gdsc.comunity.exception.ErrorCode;
 import java.io.File;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,15 +22,22 @@ public class S3FileService {
 
     private final String bucketName = "mohaji";
 
-    public S3FileService() {
-        this.s3Client = AmazonS3ClientBuilder.defaultClient();
+    public S3FileService(@Value("${aws.access-key}") String accessKeyId,
+        @Value("${aws.secret-key}") String secretKey) {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKeyId, secretKey);
+        this.s3Client = AmazonS3ClientBuilder.standard()
+            .withRegion("ap-northeast-2")
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+            .build();
     }
 
     public String uploadImage(MultipartFile image){
         String s3FileName = UUID.randomUUID().toString().substring(0, 10) + "_" + image.getOriginalFilename();
         try {
-            File convertedFile = convertMultiPartFileToFile(image);
-            s3Client.putObject(new PutObjectRequest(bucketName, s3FileName, convertedFile)
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(image.getContentType());
+            metadata.setContentLength(image.getSize());
+            s3Client.putObject(new PutObjectRequest(bucketName, s3FileName, image.getInputStream(), metadata)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (Exception e) {
             throw new CustomException(ErrorCode.FILE_UPLOAD_ERROR);
@@ -36,10 +47,12 @@ public class S3FileService {
     }
 
     public String uploadFile(MultipartFile file) {
-        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + "_" + file.getOriginalFilename();
+        String s3FileName = UUID.randomUUID().toString().substring(0, 15);
         try {
-            File convertedFile = convertMultiPartFileToFile(file);
-            s3Client.putObject(new PutObjectRequest(bucketName, s3FileName, convertedFile));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            s3Client.putObject(new PutObjectRequest(bucketName, s3FileName, file.getInputStream(), metadata));
         } catch (Exception e) {
             throw new CustomException(ErrorCode.FILE_UPLOAD_ERROR);
         }
@@ -48,22 +61,11 @@ public class S3FileService {
 
     public File downloadFile(String s3FileName) {
         try {
-            File file = File.createTempFile(s3FileName.substring(11), null);
+            File file = File.createTempFile(s3FileName, null);
             s3Client.getObject(new GetObjectRequest(bucketName, s3FileName), file);
             return file;
         } catch (Exception e) {
             throw new CustomException(ErrorCode.FILE_DOWNLOAD_ERROR);
         }
     }
-
-    private File convertMultiPartFileToFile(MultipartFile file) {
-        File convertedFile = new File(file.getOriginalFilename());
-        try {
-            file.transferTo(convertedFile);
-        } catch (Exception e) {
-            throw new CustomException(ErrorCode.FILE_TRANSFORM_ERROR);
-        }
-        return convertedFile;
-    }
-
 }
