e5bfd03874517500e950f08271a22b8ed09972dd
Gyumin Sim <gyu2781@gmail.com>
2024-05-26 00:06:50 +0900

    #9 fix : Chatting에서 LocalDateTime yyyy-MM-dd'T'HH:mm:ss 형태의 JsonFormat으로 변경

--- src/main/java/gdsc/comunity/dto/chat/Chatting.java
+++ src/main/java/gdsc/comunity/dto/chat/Chatting.java
@@ -1,5 +1,6 @@
 package gdsc.comunity.dto.chat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import gdsc.comunity.entity.channel.Channel;
 import gdsc.comunity.entity.chat.ChatLog;
 import gdsc.comunity.entity.chat.ChatType;
@@ -12,6 +13,9 @@ public class Chatting {
     public final String message;
     public final String senderNickname;
     public final ChatType type;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+        pattern = "yyyy-MM-dd'T'HH:mm:ss",
+        timezone = "Asia/Seoul")
     public final LocalDateTime time;
 
     public ChatLog toEntity(Channel channel, Long senderId, ChatType type) {
